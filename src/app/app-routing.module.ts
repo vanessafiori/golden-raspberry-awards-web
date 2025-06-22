60b9e41eda232d9fa2e285b574bf5552f3530285
vf.fiori <vf.fiori@gmail.com>
2025-06-22 20:25:34 -0300

    style: organização visual do layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { MoviesComponent } from './pages/movies/movies.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'movies', component: MoviesComponent },
   { path: 'dashboard', component: DashboardComponent },
 ];
